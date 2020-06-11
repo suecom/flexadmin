a6a68b427626aacccfd98e7fb7ed6b7b5c81d186
Allister Mannion <allister@oldencars.com>
2020-06-11 14:12:07 +0200

    Update flexsdk.js

--- src/flexsdk.js
+++ src/flexsdk.js
@@ -1,11 +1,11 @@
 import * as flexIntegrationSdk from 'sharetribe-flex-integration-sdk';
 
+console.log('Using: ', process.env.REACT_APP_FLEX_INTEGRATION_BASE_URL || 'https://flex-api.sharetribe.com')
+
 const integrationSdk = flexIntegrationSdk.createInstance({
     clientId: process.env.REACT_APP_FLEX_INTEGRATION_CLIENT_ID || '58a55f71-7650-423f-bbd3-b4f5e6000000', 
     clientSecret: process.env.REACT_APP_FLEX_INTEGRATION_CLIENT_SECRET || '14cd97d21409e22c720e58fbf872a2113f000000',
     baseUrl: process.env.REACT_APP_FLEX_INTEGRATION_BASE_URL || 'https://flex-api.sharetribe.com'
 });
-
-console.log('Using: ', process.env.REACT_APP_FLEX_INTEGRATION_BASE_URL || 'https://flex-api.sharetribe.com')
 
 export default integrationSdk;
